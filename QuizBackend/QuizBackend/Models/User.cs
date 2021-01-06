d3762123a5b8cf404eac014f9115731c07877f19
Sruthy Jayachandran <sruthy.jayachandran@experionglobal.local>
2021-01-06 13:05:36 +0530

    Quiz Application

---
+++ QuizBackend/QuizBackend/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizBackend.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Column(TypeName="nvarchar(20)")]
+        public string UserName { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Password { get; set; }
+    }
+}
